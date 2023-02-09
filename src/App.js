05cc3cfcf0c608400baff03933a98f865b781afe
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-02-10 05:47:42 +0900

    add header

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React from 'react';
+import React from 'react'
 import './App.css';
+import Header from './Components/Header'
+
 
 function App() {
   return (
-    <div className='container text-center'>
-      <Clock />
+    <div className='App'>
+      <Header />
     </div>
   )
 }
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.now = new Date();
-
-    this.state = {
-      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
-    }
-    this.refresh = this.refresh.bind(this);
-  }
-
-
-
-  refresh() {
-    this.now = new Date();
-
-    this.setState((state) => ({
-      time: `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
-    }));
-  }
-
-  render() {
-
-    return <p onClick={this.refresh}>{this.state.time}</p>
-  }
-}
-
-
-export default App;
+export default App
