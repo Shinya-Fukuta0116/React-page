98d1212a44f1829616ae699df95c571fd0bbc61a
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-02-18 04:54:58 +0900

    add img

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,11 +1,62 @@
 import React from 'react'
 import './Header.css'
+import Image from './img/couch-g68f2df79f_1920.jpg'
 
 
 const HomePage = () => {
   return (
-    <main>
-    
+    <main className='wrapper' id='top'>
+      <ul className="product-list">
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+        <li>
+          <a href="item1.html" />
+          <img src={Image} alt='Image' />
+          <p>プロダクトタイトルプロダクトタイトル<br />
+            ¥99,999 +tax</p>
+        </li>
+
+      </ul >
     </main>
   )
 }
